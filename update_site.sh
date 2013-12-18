531da63f069a8503d02597e66c241ba36b996e9b
Frederic Delbos <fred.delbos@gmail.com>
2013-12-18 20:35:54 +0100

    ok a bug!!!

--- update_site.sh
+++ update_site.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 grunt && grunt doc && git checkout gh-pages && \
+rm -fr static && rm -fr *.html && \
 mv doc/*.html . && mv doc/static . && rm -fr doc && \
 git add *.html static && git commit -m "updating site" && \
 git push origin gh-pages  && git checkout master
