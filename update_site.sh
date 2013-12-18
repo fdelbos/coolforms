7389216761eb273d84ef35a402cc4a4f5d4462d3
Frederic Delbos <fred.delbos@gmail.com>
2013-12-18 20:39:21 +0100

    back to master

--- update_site.sh
+++ update_site.sh
@@ -4,4 +4,5 @@ grunt && grunt doc && git checkout gh-pages && \
 rm -fr static && rm -fr *.html && \
 mv doc/*.html . && mv doc/static . && rm -fr doc && \
 git add *.html static && git commit -m "updating site" && \
-git push origin gh-pages  && git checkout master
+git push origin gh-pages
+git checkout master
